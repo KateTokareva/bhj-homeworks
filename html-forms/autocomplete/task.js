a619e0a37931bba5dde7fc3ebd84222970529283
Katerina Tokareva <zaryadnova.e@mail.ru>
2023-08-09 14:37:08 +0300

    first commit autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,26 +68,18 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
-  }
-}
+    let arr = [];
+    let options = this.input.options;
+    for (let item of options) {
+      if (item.innerText.toLowerCase().includes(text.toLowerCase())) {
+        arr.push({
+          text: item.innerText,
+          value: item.getAttribute('value')
+        });
+      };
+    };
+    return arr;
+  };
+};
 
 new Autocomplete( document.querySelector( '.autocomplete' ));
