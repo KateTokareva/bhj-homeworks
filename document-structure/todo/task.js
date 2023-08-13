196d603e8e9f5df53db36e947be794a2108ef163
Katerina Tokareva <zaryadnova.e@mail.ru>
2023-08-13 23:37:12 +0300

    added the trim() method, removed the event handler

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -4,23 +4,23 @@ let taskList = document.getElementById('tasks__list');
 let form = document.getElementById('tasks__form');
 
 
-function addTask (event) {
-    event.preventDefault();
-    let newTask = input.value;
-    if (input.value) taskList.insertAdjacentHTML('beforeend', `<div class="task"> <div class="task__title"> ${newTask} </div> <a href="#" class="task__remove">&times;</a> </div>`);
-    input.value = '';
+function addTask(event) {
+	event.preventDefault();
+	let newTask = input.value;
+	if (newTask.trim()) {
+		taskList.insertAdjacentHTML('beforeend', `<div class="task"> <div class="task__title"> ${newTask} </div> <a href="#" class="task__remove">&times;</a> </div>`);
+	};
+	input.value = '';
 };
 
-function delateTask (event) {
-    event.preventDefault();
-    let button = event.target;
-    if (button.classList.contains('task__remove')) {
-      let parent = button.closest('.task');
-      parent.remove();
-   };
+function delateTask(event) {
+	event.preventDefault();
+	let button = event.target;
+	if (button.classList.contains('task__remove')) {
+		let parent = button.closest('.task');
+		parent.remove();
+	};
 };
 
-btn.addEventListener ('click', addTask);
-form.addEventListener ('submit', addTask);
+form.addEventListener('submit', addTask);
 taskList.addEventListener('click', delateTask);
-
